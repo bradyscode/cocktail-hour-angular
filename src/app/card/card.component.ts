b4181f438005a94638f7c6f6ee990d115d280d98
BradysCode <bradystohler15@gmail.com>
2023-07-11 06:36:04 -0400

    Add serivce for drinks api

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { DrinkServiceService } from '../drink-service.service';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  
+  
 }
