516dcb6ed7a873d6a4dba750696a1e27e25d2099
BradysCode <bradystohler15@gmail.com>
2023-07-12 12:16:33 -0400

    fix api call, implement into card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { DrinkServiceService } from '../drink-service.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  
+  constructor(private ds: DrinkServiceService){}
+
+  getDrink(){
+    this.ds.getRandomDrink().subscribe(val => console.log(val));
+  }
   
 }
--- src/app/drink-service.service.ts
+++ src/app/drink-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { Drink } from './cocktail.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +9,15 @@ import { HttpClient } from '@angular/common/http';
 export class DrinkServiceService {
 
   constructor(private http: HttpClient) { }
-
   rootURL = 'https://www.thecocktaildb.com/api/json/v1/1';
 
-  getRandomDrink() {
-    return this.http.get(this.rootURL + '/random.php');
+   getRandomDrink(): Observable<Drink> {
+    return this.http.get<any>(`${this.rootURL}/random.php`).pipe(
+      map(drink => {
+        return drink;
+      }),
+      catchError(error => throwError(error))
+    );
   }
 }
+
