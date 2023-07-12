553f490a2e659cf3f30672195ae39274e9655795
BradysCode <bradystohler15@gmail.com>
2023-07-12 12:20:31 -0400

    assign drink variable in card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DrinkServiceService } from '../drink-service.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Drink } from '../cocktail.service';
 
 @Component({
   selector: 'app-card',
@@ -9,9 +10,12 @@ import { HttpClient, HttpHandler } from '@angular/common/http';
 })
 export class CardComponent {
   constructor(private ds: DrinkServiceService){}
-
+  private drink: Drink;
   getDrink(){
-    this.ds.getRandomDrink().subscribe(val => console.log(val));
+    this.ds.getRandomDrink().subscribe(res => {
+      this.drink = res;
+      console.log(this.drink);
+    })
   }
   
 }
